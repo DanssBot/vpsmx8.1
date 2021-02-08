dd0bbf7443433f8858256f7d283c68e5480688f8
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-02-08 12:37:27 -0500

    Update trojanserver.sh

--- REPOSITORY/trojanserver.sh
+++ REPOSITORY/trojanserver.sh
@@ -50,10 +50,42 @@ read -p " "
 menu
 fi
 }
+function goinst () {
+echo -e "A continuacion se instalara el paquete GO Lang"
+echo -e "Continuar?"
+while [[ ${yesno} != @(s|S|y|Y|n|N) ]]; do
+read -p "[S/N]: " yesno
+tput cuu1 && tput dl1
+done
+if [[ ${yesno} = @(s|S|y|Y) ]]; then
+echo -e "Instalando..."
+cd
+rm -rf /usr/local/go 1> /dev/null 2> /dev/null
+wget https://golang.org/dl/go1.15.linux-amd64.tar.gz 1> /dev/null 2> /dev/null
+tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
+if cat /root/.bashrc | grep GOROOT; then
+echo -e "Ya existe un GoROOT - Skipping"
+else
+sed -i '$a export GOROOT=/usr/local/go' /root/.bashrc
+sed -i '$a export GOPATH=$HOME/go' /root/.bashrc
+sed -i '$a export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' /root/.bashrc
+rm go1.15.linux-amd64.tar.gz
+source /root/.bashrc
+fi
+sleep 5
+echo -e "Reiniciando Fuente de Terminal..."
+fi
+echo -e "\033[1;33m Para finalizar el proceso de Instalacion de GO escribe :\033[0m"
+echo -e "\033[1;32m source ~/.profile\033[0m"
+echo -e "\033[1;31mPRESIONE ENTER PARA CONTINUAR\033[0m"
+read -p " "
+exit 0
+}
 
 msg -ama "$(fun_trans "MENU DE UTILITARIOS")"
 msg -bar
 echo -ne "\033[1;32m [1] > " && msg -azu "$(fun_trans "INSTALAR TROJAN SERVER") "
+echo -ne "\033[1;32m [2] > " && msg -azu "$(fun_trans "INSTALAR Go Lang") "
 msg -bar
 while [[ ${arquivoonlineadm} != @(0|[1-5]) ]]; do
 read -p "[0-5]: " arquivoonlineadm
@@ -61,5 +93,6 @@ tput cuu1 && tput dl1
 done
 case $arquivoonlineadm in
 1)trojanserver;;
+2)function goinst;;
 esac
 msg -bar
