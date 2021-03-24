0f7deb55c17b29a0e869184eec7c97f20616547f
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-03-24 10:13:44 -0500

    Update v2ray.sh

--- REPOSITORY/v2ray.sh
+++ REPOSITORY/v2ray.sh
@@ -145,6 +145,54 @@ sed -i "${resta}d" /etc/v2ray/config.json
 v2ray restart > /dev/null 2>&1
 msg -bar
 }
+mosusr_kk() {
+clear 
+clear
+msg -bar
+msg -tit
+msg -ama "         USUARIOS REGISTRADOS | UUID V2RAY"
+msg -bar
+# usersss=$(cat /etc/VPS-MX/RegV2ray|cut -d '|' -f1)
+# cat /etc/VPS-MX/RegV2ray|cut -d'|' -f3
+VPSsec=$(date +%s)
+local HOST="/etc/RegV2ray"
+local HOST2="/etc/RegV2ray"
+local RETURN="$(cat $HOST|cut -d'|' -f2)"
+local IDEUUID="$(cat $HOST|cut -d'|' -f1)"
+if [[ -z $RETURN ]]; then
+echo -e "----- NINGUN USER REGISTRADO -----"
+msg -ne "Enter Para Continuar" && read enter
+${SCPinst}/v2ray.sh
+
+else
+i=1
+echo -e "\e[97m                 UUID                | USER | EXPIRACION \e[93m"
+msg -bar
+while read hostreturn ; do
+DateExp="$(cat /etc/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f3)"
+if [[ ! -z $DateExp ]]; then             
+DataSec=$(date +%s --date="$DateExp")
+[[ "$VPSsec" -gt "$DataSec" ]] && EXPTIME="\e[91m[EXPIRADO]\e[97m" || EXPTIME="\e[92m[$(($(($DataSec - $VPSsec)) / 86400))]\e[97m Dias"
+else
+EXPTIME="\e[91m[ S/R ]"
+fi 
+usris="$(cat /etc/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f2)"
+local contador_secuencial+="\e[93m$hostreturn \e[97m|\e[93m$usris\e[97m|\e[93m $EXPTIME \n"           
+      if [[ $i -gt 30 ]]; then
+	      echo -e "$contador_secuencial"
+	  unset contador_secuencial
+	  unset i
+	  fi
+let i++
+done <<< "$IDEUUID"
+
+[[ ! -z $contador_secuencial ]] && {
+linesss=$(cat /etc/RegV2ray | wc -l)
+	      echo -e "$contador_secuencial \n Numero de Registrados: $linesss"
+	}
+fi
+msg -bar
+}
 msg -ama "$(fun_trans "MENU V2RAY")"
 msg -bar
 echo -ne "\033[1;32m [1] > " && msg -azu "$(fun_trans "INSTALAR V2RAY") "
@@ -155,10 +203,11 @@ echo -ne "\033[1;32m [5] > " && msg -azu "$(fun_trans "INFORMACION DE CUENTA")"
 echo -ne "\033[1;32m [6] > " && msg -azu "$(fun_trans "DESINTALAR V2RAY")"
 echo -ne "\033[1;32m [7] > " && msg -azu "$(fun_trans "AGREGAR USUARIO UUID")"
 echo -ne "\033[1;32m [8] > " && msg -azu "$(fun_trans "ELIMINAR USUARIO UUID")"
+echo -ne "\033[1;32m [9] > " && msg -azu "$(fun_trans "MOSTAR USUARIOS REGISTRADOS")"
 msg -bar && echo -ne "$(msg -verd "[0]") $(msg -verm2 ">") "&& msg -bra "\033[1;41mREGRESAR AL MENU"
 msg -bar
-while [[ ${arquivoonlineadm} != @(0|[1-8]) ]]; do
-read -p "[0-8]: " arquivoonlineadm
+while [[ ${arquivoonlineadm} != @(0|[1-9]) ]]; do
+read -p "[0-9]: " arquivoonlineadm
 tput cuu1 && tput dl1
 done
 case $arquivoonlineadm in
@@ -170,6 +219,7 @@ case $arquivoonlineadm in
 6)unistallv2;;
 7)addusr;;
 8)delusr;;
+9)mosusr_kk;;
 0)exit;;
 esac
 msg -bar
